1ccfa02fd09f2c1845ffc3b990c920876ffa805c
caritobd91 <cborman@doeanderson.com>
2020-04-15 13:25:08 -0400

    [add] svg sprite and base icon component

---
+++ vue-playground/src/components/BaseIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <svg :width="width" :height="height">
+      <use v-bind="{ 'xlink:href':'/feather-sprite.svg#'+name}" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  props: {
+    name: String,
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    height: {
+      type: [Number, String],
+      default: 24
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vue-playground/src/components/EventCard.vue
+++ vue-playground/src/components/EventCard.vue
@@ -3,28 +3,34 @@
   <div>
     <span>@{{ event.time }} on {{ event.date }}</span>
     <h4>{{ event.title }}</h4>
+    <BaseIcon name="users" />
     <span>{{ event.attendees.length }} attending</span>
   </div>
 </router-link>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        event: {
-          id: 1,
-          title: 'Park Cleanup',
-          date: 'Tuesday',
-          time: '6:00',
-          attendees: [
-            { id: 'abc123', name: 'Adam' },
-            { id: 'de456', name: 'Gregg' },
-          ]
-        }
+import BaseIcon from '@/components/BaseIcon.vue';
+
+export default {
+  components: {
+    BaseIcon,
+  },
+  data() {
+    return {
+      event: {
+        id: 1,
+        title: 'Park Cleanup',
+        date: 'Tuesday',
+        time: '6:00',
+        attendees: [
+          { id: 'abc123', name: 'Adam' },
+          { id: 'de456', name: 'Gregg' },
+        ]
       }
-    },
-  }
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped>
