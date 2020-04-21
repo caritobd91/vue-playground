c0e6167029c6d02f5852488590a0aaa129380611
caritobd91 <cborman@doeanderson.com>
2020-04-21 10:07:20 -0400

    [update] accesing state using Vuex

--- vue-playground/src/views/EventCreate.vue
+++ vue-playground/src/views/EventCreate.vue
@@ -1,5 +1,53 @@
 <template>
   <div>
-    Event Create
+    <h1>Create an event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">
+        {{ cat }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+// mapState allows you to map the state into computed
+import { mapState } from "vuex";
+
+export default {
+  // NOTE: using spread operator ...mapState({}) allows you to add local computed properties
+  // computed: {
+  //   localComputed() {
+  //     return something
+  //   },
+  //   ...mapState(["user", "categories"])
+  // }
+
+  // NOTE: cleaner way to use mapState, this way it needs to use dot notation in the markup, instead of computed function name
+  computed: mapState({
+    user: "user",
+    categories: "categories"
+  })
+
+  // NOTE: Using mapState to access the store data
+  // computed: mapState({
+  //   userName: state => state.user.name,
+  //   userID: state => state.user.id,
+  //   categories: state => state.categories
+  // })
+
+  // NOTE: How to access name and id from the store using computed properties without mapState:
+  // computed: {
+  //   userName() {
+  //     return this.$store.state.user.name;
+  //   },
+  //   userId() {
+  //     return this.$store.state.user.id;
+  //   }
+  // }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
